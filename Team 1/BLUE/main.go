7fc525bbeae96ab3ed3930447241471e682e65ee
Ethan Sturek <ethansturek@gmail.com>
2026-03-10 14:02:28 -0700

    Added Go script for defense

---
+++ Team 1/BLUE/main.go
@@ -0,0 +1,13 @@
+// 1. Package Declaration
+package main
+
+// 2. Imports (The tools we are borrowing)
+import (
+	"fmt" //printing to console
+)
+
+// 3. The Main Function (The entry point)
+func main() {
+	fmt.Println("GoGuard IPS: Defender Node is starting...")
+	fmt.Println("Interface: enp0s8 (Target)")
+}
